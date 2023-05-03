1b820b2dc65e434e2cf702ac0ad00434bf80e3f8
erico cavalcanti <ecocaval@hotmail.com>
2023-05-03 02:02:37 -0300

    fix: server health controller

--- src/main/java/com/petzzz/app/controllers/HealthController.java
+++ src/main/java/com/petzzz/app/controllers/HealthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/health")
 public class HealthController {
@@ -21,8 +20,6 @@ public class HealthController {
 
     @GetMapping
     public ResponseEntity<HealthCheckResponse> healthCheck() {
-        return ResponseEntity.ok(
-                healthCheckResponse.build("Health Checked!")
-        );
+        return ResponseEntity.ok(healthCheckResponse.assemble("Health Checked!"));
     }
 }
--- src/main/java/com/petzzz/app/responses/HealthCheckResponse.java
+++ src/main/java/com/petzzz/app/responses/HealthCheckResponse.java
@@ -1,6 +1,7 @@
 package com.petzzz.app.responses;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,13 +10,14 @@ import java.time.LocalDateTime;
 
 @Component
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class HealthCheckResponse {
     private String status;
     private LocalDateTime timestamp;
 
-    public HealthCheckResponse build(String status) {
+    public HealthCheckResponse assemble(String status) {
         this.status = status;
         this.timestamp = LocalDateTime.now();
         return this;
